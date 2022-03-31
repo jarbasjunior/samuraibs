2450bedc7e727afb39386b1f8c19184c6c4ba55f
jarbasjunior <jarbasoujunior@gmail.com>
2022-03-31 17:04:49 -0300

    adjust_test: added contexts

--- tests/cypress/integration/singup.spec.js
+++ tests/cypress/integration/singup.spec.js
@@ -1,17 +1,44 @@
-it('Deve cadastrar um novo usuário', () => {
-  const user = { name: 'Jarbas Junior', email: 'jarbas.junior@samuraibs.com.br' };
-  cy.task('removeUser', user.email);
+describe('Dado que acesso a página de cadastro', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.contains('a', 'Criar conta').click();
+  });
 
-  cy.visit('/');
-  cy.contains('a', 'Criar conta').click();
+  context('Quando o usuário não tiver e-mail cadastrado', () => {
+    const user = { name: 'Jarbas Junior', email: 'jarbas.junior@samuraibs.com.br', password: '123456' };
+    before(() => cy.task('removeUser', user.email));
 
-  cy.get('input[placeholder="Nome"]').type(user.name);
-  cy.get('input[placeholder="E-mail"]').type(user.email);
-  cy.get('input[placeholder="Senha"]').type('123456');
-  cy.contains('button', 'Cadastrar').click();
+    it('Deve permitir cadastrar novo usuário com sucesso', () => {
+      cy.get('input[placeholder="Nome"]').type(user.name);
+      cy.get('input[placeholder="E-mail"]').type(user.email);
+      cy.get('input[placeholder="Senha"]').type(user.password);
+      cy.contains('button', 'Cadastrar').click();
 
-  cy.get('.toast').should('be.visible').find('p')
-    .should('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!');
+      cy.get('.toast').should('be.visible').find('p')
+        .should('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!');
+      cy.get('.gmtmqV').should('be.visible');
+    });
+  });
 
-  cy.get('.gmtmqV').should('be.visible');
+  context('Quando usuário já tiver e-mail cadastrado', () => {
+    const user = {
+      name: 'Novo usuário', email: 'novo.usuario@samuraibs.com.br', password: '123456', is_provider: true,
+    };
+    before(() => {
+      cy.task('removeUser', user.email);
+      cy.request('POST', 'http://localhost:3333/users', user)
+        .then((result) => expect(result.status).to.eq(200));
+    });
+
+    it('Deve proibir cadastro de novo usuário', () => {
+      cy.get('input[placeholder="Nome"]').type(user.name);
+      cy.get('input[placeholder="E-mail"]').type(user.email);
+      cy.get('input[placeholder="Senha"]').type(user.password);
+      cy.contains('button', 'Cadastrar').click();
+
+      cy.get('.toast').should('be.visible').find('p')
+        .should('have.text', 'Email já cadastrado para outro usuário.');
+      cy.get('.gmtmqV').should('not.exist');
+    });
+  });
 });
