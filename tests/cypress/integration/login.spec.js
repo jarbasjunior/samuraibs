b75b7d6a9caad69ef6b9d7272416cdf87bc3c6e6
jarbasjunior <jarbasoujunior@gmail.com>
2022-04-02 10:40:20 -0300

    test: must deny login with wrong password

--- tests/cypress/integration/login.spec.js
+++ tests/cypress/integration/login.spec.js
@@ -1,5 +1,6 @@
 import SigninPage from '../support/pages/signin/signin_page';
 import DashHeader from '../support/components/headers/dash_header';
+import Toast from '../support/components/toasts/toasts';
 
 describe('Dado que acesso a página de login', () => {
   context('Quando o usuário tem cadastro', () => {
@@ -17,5 +18,24 @@ describe('Dado que acesso a página de login', () => {
     });
 
     after(() => cy.clearLocalStorage());
+  });
+
+  context('Quando usuário tentar fazer login com a senha errada', () => {
+    const user = {
+      name: 'Novo usuário', email: 'novo.usuario@samuraibs.com.br', password: '123456', is_provider: true,
+    };
+
+    before(() => {
+      cy.postUser(user).then(() => {
+        user.password = '654321';
+      });
+    });
+
+    it('Deve exibir mensagem de erro', () => {
+      SigninPage.go();
+      SigninPage.fillForm(user);
+      SigninPage.submitForm();
+      Toast.mustHaveText('Ocorreu um erro ao fazer login, verifique suas credenciais.');
+    });
   });
 });
