e194573dd8a980cfc8e9c6bf1b5f352b45d06e1d
jarbasjunior <jarbasoujunior@gmail.com>
2022-04-02 09:17:18 -0300

    adjust_test: remove token after login test validation

--- tests/cypress/integration/login.spec.js
+++ tests/cypress/integration/login.spec.js
@@ -2,7 +2,7 @@ import SigninPage from '../support/pages/signin/signin_page';
 import DashHeader from '../support/components/headers/dash_header';
 
 describe('Dado que acesso a página de login', () => {
-  context.only('Quando o usuário tem cadastro', () => {
+  context('Quando o usuário tem cadastro', () => {
     const user = {
       name: 'Novo usuário', email: 'novo.usuario@samuraibs.com.br', password: '123456', is_provider: true,
     };
@@ -19,5 +19,7 @@ describe('Dado que acesso a página de login', () => {
       SigninPage.submitForm();
       DashHeader.mustHaveName(user.name);
     });
+
+    after(() => cy.clearLocalStorage());
   });
 });
